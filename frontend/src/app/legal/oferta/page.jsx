4ef1cccfa0519937ac7486bb56057218535f8de5
Vladislav <sunceetpochta@mail.ru>
2025-07-22 15:26:06 +0900

    added pt-15

--- frontend/src/app/legal/oferta/page.jsx
+++ frontend/src/app/legal/oferta/page.jsx
@@ -41,7 +41,7 @@ export default function OfertaPage() {
 
   return (
     <NoSSR>
-      <div className="min-h-screen bg-gray-50">
+      <div className="min-h-screen pt-15 bg-gray-50">
         <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
           {/* Заголовок */}
           <div className="text-center mb-6 sm:mb-12">
